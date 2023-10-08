db9e9b11491441e5f2842cd6cac94a55fb930c4e
Filipe <filipe-all@hotmail.com>
2023-10-07 21:04:58 -0300

    Feat: Modal to create channel

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -1,15 +1,16 @@
 "use client";
 import * as z from "zod";
+import qs from "query-string";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/navigation";
+import { ChannelType } from "@prisma/client";
+import { useParams, useRouter } from "next/navigation";
 import {
     Dialog,
     DialogContent,
     DialogHeader,
     DialogTitle,
-    DialogDescription,
     DialogFooter
 } from "@/components/ui/dialog";
 import {
@@ -20,37 +21,53 @@ import {
     FormLabel,
     FormMessage
 } from "@/components/ui/form";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue
+} from "@/components/ui/select"
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { FileUpload } from "@/components/file-upload";
 import { useModal } from "@/hooks/use-modal-store";
 
 const formSchema = z.object({
     name: z.string().min(1, {
-        message: "Server name is required."
-    }),
-    imageUrl: z.string().min(1, {
-        message: "Server image is required."
-    })
+        message: "Channel name is required."
+    }).refine(
+        name => name !== "general",
+        {
+            message: "Channel can notbe 'general'"
+        }
+    ),
+    type: z.nativeEnum(ChannelType)
 })
-export const CreateServerModal = () => {
+export const CreateChannelModal = () => {
     const { isOpen, onClose, type } = useModal();
     const router = useRouter();
+    const params = useParams()
 
-    const isModalOpen = isOpen && type === "createServer";
+    const isModalOpen = isOpen && type === "createChannel";
 
     const form = useForm ({
         resolver: zodResolver(formSchema),
         defaultValues: {
             name: "",
-            imageUrl: "",
+            type: ChannelType.TEXT
         }
     });
 
     const isLoading = form.formState.isSubmitting;
     const onSubmit = async(values: z.infer<typeof formSchema>) => {
         try {
-            await axios.post("/api/servers", values);
+            const url = qs.stringifyUrl({
+                url: "/api/channels",
+                query: {
+                    serverId: params?.serverId
+                }
+            });
+            await axios.post(url, values);
             router.refresh();
             handleClose()
         } catch (error) {
@@ -68,32 +85,12 @@ export const CreateServerModal = () => {
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
                     <DialogTitle className="text-2xl text-center font-bold">
-                        Customize your server
+                        Create Channel
                     </DialogTitle>
-                    <DialogDescription className="text-center text-zinc-500">
-                        Give your server a personality with a name and image. You can always change it later.
-                    </DialogDescription>
                 </DialogHeader>
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
-                            <div className="flex items-center justify-center text-center">
-                                <FormField 
-                                    control={form.control}
-                                    name="imageUrl"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormControl>
-                                               <FileUpload 
-                                                    endpoint="serverImage"
-                                                    value={field.value}
-                                                    onChange={field.onChange}
-                                               />
-                                            </FormControl>
-                                        </FormItem>
-                                    )}
-                                />
-                            </div>                        
                             <FormField 
                                 control={form.control}
                                 name="name"
@@ -102,16 +99,52 @@ export const CreateServerModal = () => {
                                         <FormLabel
                                             className="uppercase text-xs font-bold text-zinc-500 dark: text-secondary/70"
                                         >
-                                            Server name
+                                            Channel name
                                         </FormLabel>
                                         <FormControl>
                                             <Input 
                                                 disabled={isLoading}
                                                 className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                                                placeholder="Enter server name"
+                                                placeholder="Enter channel name"
                                                 {...field}
                                             />
                                         </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="type"
+                                render={( { field }) => (
+                                    <FormItem>
+                                        <FormLabel>Channel Type</FormLabel>
+                                        <Select
+                                            disabled={isLoading}
+                                            onValueChange={field.onChange}
+                                            defaultValue={field.value}
+                                        >
+                                            <FormControl>
+                                                <SelectTrigger
+                                                    className="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none"
+                                                >
+                                                    <SelectValue
+                                                        placeholder="Select a channel type"
+                                                    />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                {Object.values(ChannelType).map((type) => (
+                                                    <SelectItem
+                                                        key={type}
+                                                        value={type}
+                                                        className="capitalize"
+                                                    >
+                                                        {type.toLowerCase()}
+                                                    </SelectItem>
+                                                ))}
+                                            </SelectContent>
+                                        </Select>
                                         <FormMessage />
                                     </FormItem>
                                 )}
