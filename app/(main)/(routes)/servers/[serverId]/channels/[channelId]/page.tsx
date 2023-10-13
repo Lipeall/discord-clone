c1aa016cdd42b3a8ee5bba247421e20c79ca0696
Filipe <filipe-all@hotmail.com>
2023-10-13 18:46:30 -0300

    Feat: Chat input

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -3,44 +3,55 @@ import { RedirectToSignIn } from "@clerk/nextjs";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import ChatHeader from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
     params: {
         serverId: string;
         channelId: string;
-    }
+    };
 }
 
-const ChannelIdPage = async({ params }: ChannelIdPageProps) => {
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
     const profile = await currentProfile();
 
-    if(!profile) {
-        return <RedirectToSignIn />
+    if (!profile) {
+        return <RedirectToSignIn />;
     }
     const channel = await db.channel.findUnique({
         where: {
             id: params.channelId,
-        }
+        },
     });
-    
+
     const member = await db.member.findMany({
         where: {
             profileId: profile.id,
-        }
+        },
     });
 
-    if(!channel || !member) {
-        redirect("/")
+    if (!channel || !member) {
+        redirect("/");
     }
-    return ( 
-        <div>
-            <ChatHeader 
+    return (
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader
                 name={channel.name}
                 serverId={channel.serverId}
                 type="channel"
             />
+            <div className="flex-1">Future Messages</div>
+            <ChatInput
+                apiUrl="/api/socket/messages"
+                name={channel.name}
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId,
+                }}
+                type="channel"
+            />
         </div>
-     );
-}
- 
+    );
+};
+
 export default ChannelIdPage;
